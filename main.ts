ba18fd016f6dba4ec3b4451a74ce1cd446efd821
phrynus <fondantcn@gmail.com>
2025-02-04 21:12:38 +0800

    基础

---
+++ main.ts
@@ -0,0 +1,6 @@
+// bun build --compile --target=bun-windows-x64-baseline --minify --sourcemap --bytecode main.ts --outfile myapp
+// bun build --compile --target=bun-linux-x64-baseline --minify --sourcemap --bytecode main.ts --outfile myapp
+setInterval(() => {
+    console.log('hello world')
+},0)
+
